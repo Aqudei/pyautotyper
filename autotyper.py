9fca13adb4cf826bb28fcb21ae1514c40a18058b
aqudei <fdeu.aalmine@gmail.com>
2024-08-31 00:20:32 +0800

    on demand settings read

--- autotyper.py
+++ autotyper.py
@@ -7,6 +7,10 @@ import pyautogui
 import yaml
 import schedule
 import logging
+import traceback as tb
+import hashlib
+from PIL import Image
+
 
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -36,8 +40,37 @@ file_handler.setFormatter(file_format)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+CONFIG_FILE = "./input.yaml"
+ORIGINAL_RESOLUTION = (2560, 1440) # Resolution of my Laptop
+
+def resize_image_for_resolution(sample_image_path, original_resolution, current_resolution):
+    # Open the sample image
+    sample_image = Image.open(sample_image_path)
+
+    # Calculate the scaling factor
+    width_ratio = current_resolution[0] / original_resolution[0]
+    height_ratio = current_resolution[1] / original_resolution[1]
+
+    # Resize the sample image based on the current resolution
+    new_width = int(sample_image.width * width_ratio)
+    new_height = int(sample_image.height * height_ratio)
+    resized_image = sample_image.resize((new_width, new_height))
+
+    return resized_image
+
+def compute_checksum(file_path, algorithm='md5'):
+    hash_func = hashlib.new(algorithm)
+    
+    # Read the file in chunks to avoid memory overload with large files
+    with open(file_path, 'rb') as file:
+        while chunk := file.read(8192):
+            hash_func.update(chunk)
+    
+    return hash_func.hexdigest()
+
 def read_params():
-    with open("./input.yaml",'r') as infile:
+
+    with open(CONFIG_FILE,'r') as infile:
         params = yaml.load(infile,Loader=Loader)
         return params
 
@@ -47,8 +80,37 @@ def find_window(window_name):
         if not window_name.lower() in w.title.lower():
             continue        
         return w
+
+def ensure_unlocked(password):
+    current_resolution = pyautogui.size()
+    mouse_x,mouse_y = pyautogui.position()
+    pyautogui.moveTo(2,2,duration=1)
     
-def job(params):
+    try:
+        login_window = find_window("Workstation is Locked by")
+        if not login_window:
+            return
+        
+        if login_window.title != gw.getActiveWindow().title:
+            login_window.activate()
+
+        resized = resize_image_for_resolution("./images/login_switch.png",ORIGINAL_RESOLUTION,current_resolution)
+        # Save the resized image or use it directly
+        
+        login_x,login_y = pyautogui.locateCenterOnScreen(resized, grayscale=True)
+        pyautogui.click(login_x,login_y)
+        
+        pyautogui.write(f"{password}")
+        pyautogui.press("enter")
+        
+        
+    except Exception as e:
+        logger.exception(e)
+        logger.error(tb.format_exc())
+    finally:
+        pyautogui.moveTo(mouse_x,mouse_y)
+    
+def perform_type_job(params):
     # Get current time without microseconds
     time_now = datetime.datetime.now().replace(microsecond=0)
 
@@ -57,6 +119,8 @@ def job(params):
         print("Not Running. Current time is outside specified range of <start> and <end>.")
         return
 
+    ensure_unlocked(params.get('password'))
+    
     # Calculate minutes delta and check divisibility by interval
     delta_minutes = (time_now - params['start']).total_seconds() // 60
     if delta_minutes % params['interval'] != 0:
@@ -77,24 +141,41 @@ def job(params):
     # Perform action on the found/active window
     print(f"{time_now} - performing type...")
     pyautogui.write(params['to_type'])
+    pyautogui.press("enter")
     
 if __name__ == "__main__":
     
     try:
+        current_resolution = pyautogui.size()  # (width, height)
+        print(f"Current Resolution: {current_resolution}")
+        
         print("Reading params...")
         params = read_params()
+        last_checksum = compute_checksum(CONFIG_FILE)
         pprint.pprint(params)
         
-        
-        schedule.every().minute.at(":05").do(job, params=params)
+        job = schedule.every().minute.at(":05").do(perform_type_job, params=params)
         
         print("Waiting...")
         while True:
+            checksum = compute_checksum(CONFIG_FILE)
+            if checksum != last_checksum:
+                print("Settings chenged! Re-initializing program...")
+                last_checksum = checksum
+                params = read_params()
+                pprint.pprint(params)
+
+                schedule.cancel_job(job)
+                job = schedule.every().minute.at(":05").do(perform_type_job, params=params)
+                print("Waiting...")
+                
             schedule.run_pending()
             time.sleep(1)
+    except KeyboardInterrupt:
+        exit(0)
     except Exception as e:
         logger.exception(e)
-    
+        logger.error(tb.format_exc())
     
     os.system("PAUSE")
 
